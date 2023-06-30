0be005c3efb43be82627a97fc3b51d66bef10c99
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-06-30 11:11:06 +0200

    Part 2 - find 3 numbers that sum up to 2020

--- 01_expense_report.py
+++ 01_expense_report.py
@@ -14,11 +14,27 @@ expenses = [int(entry.strip("/n")) for entry in input_data]
 
 # find complementaries to 2020
 
+def find_two(num, expenses):
+    for entry in expenses:
+        complementary = num-entry
+        if complementary in expenses:
+            return entry * complementary
+
+print("\nThe result is:", find_two(2020, expenses))
+
+
+
+#****************** Part 2 *****
+
+
+# find 3 parts of 2020
+
 result = 0
 
 for entry in expenses:
     complementary = 2020-entry
-    if complementary in expenses:
-        result = entry * complementary
+    temp = find_two(complementary, expenses)
+    if temp:
+        result = entry * temp
 
-print("\nThe result is:", result)
+print("\nThe new result is:", result)
