cdc1631151c95d96c7e1b1a581affa0adfa27efd
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-07-03 16:20:45 +0200

    calculate how many trees will be encountered on the toboggan trajectory

--- 03_trajectory.py
+++ 03_trajectory.py
@@ -10,8 +10,19 @@ input_data = None
 with open("03_trajectory_input.txt") as input:
     input_data = input.readlines()
 
-tree_map = [row.strip()*3 for row in input_data]
+tree_map = [row.strip()*100 for row in input_data]
 
-for line in tree_map:
-    print(line)
+# calculate trees encountered on trajectory
 
+trees_encountered = 0
+
+current_row = 0
+current_col = 0
+
+for i in range(len(tree_map)-1):
+    current_row += 1
+    current_col += 3
+    if tree_map[current_row][current_col] == "#":
+        trees_encountered += 1
+
+print("\n Number of trees encountered on trajectory:", trees_encountered)
