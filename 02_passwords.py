5199a9aee3491c4915c2dfc8d39ee71e4e464fd2
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-07-03 15:41:48 +0200

    find the number of valid passwords

--- 02_passwords.py
+++ 02_passwords.py
@@ -13,25 +13,28 @@ input_data = None
 with open("02_passwords_input.txt") as input:
     input_data = input.readlines()
 
-print(input_data)
-
 instructions_passwords = [item.strip("\n").split(": ") for item in input_data]
 
 for item in instructions_passwords:
     item[0] = item[0].split()
     item[0][0] = item[0][0].split("-")
-    for pos in item[0][0]:
-        pos = int(pos)
-
-print(instructions_passwords)
 
 InstrPw = namedtuple("InstrPw", ["min", "max", "char", "pw"])
         
 instr_pws = list()
 
 for instr_pw in instructions_passwords:
-    instr_pws.append(InstrPw(instr_pw[0][0][0], instr_pw[0][0][1], instr_pw[0][1], instr_pw[1]))
+    instr_pws.append(InstrPw(int(instr_pw[0][0][0]), int(instr_pw[0][0][1]), instr_pw[0][1], instr_pw[1]))
 
-for item in instr_pws:
-    print(tuple(item))
+# count how many passwords are valid
+
+valid_count = 0
+
+for entry in instr_pws:
+    char_count = entry.pw.count(entry.char)
+    if char_count >= entry.min and char_count <= entry.max:
+        valid_count += 1
+
+print("\nThe total number of valid passwords is:", valid_count)
+
 
