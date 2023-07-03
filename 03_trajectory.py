3ebab088111f3e5d3f6b69bc928094ad7c476d8d
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-07-03 16:46:10 +0200

    Part 2 - calculate trees encountered for different trajectories

--- 03_trajectory.py
+++ 03_trajectory.py
@@ -25,4 +25,31 @@ for i in range(len(tree_map)-1):
     if tree_map[current_row][current_col] == "#":
         trees_encountered += 1
 
-print("\n Number of trees encountered on trajectory:", trees_encountered)
+print("\nNumber of trees encountered on trajectory:", trees_encountered)
+
+
+
+#****************** Part 1 *****
+
+
+#create function to calculate trees encountered
+
+def locate_trees(map, right_plus, down_plus):
+    trees_encountered = 0
+    current_row = 0
+    current_col = 0
+    for i in range(len(map)//down_plus-1):
+        current_row += down_plus
+        current_col += right_plus
+        if map[current_row][current_col] == "#":
+            trees_encountered += 1
+    return trees_encountered
+
+trees_multiplied = 1
+
+instructions = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+for instruction in instructions:
+    trees_encountered = locate_trees(tree_map, instruction[0], instruction[1])
+    trees_multiplied *= trees_encountered
+
+print("\nYou encounter a whole lot of trees, namely:", trees_multiplied)
