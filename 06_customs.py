6e2988277872f3392813f1f166368b5c4ac19609
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-07-04 14:14:34 +0200

    Part 2 -- calculate sum of all yes answers according to new rules

--- 06_customs.py
+++ 06_customs.py
@@ -12,8 +12,30 @@ with open("06_customs_input.txt") as input:
 
 # calculate sum of yes answers
 
-combined_yes_answers = [len(set(person).difference(set("\n"))) for person in input_data]
+combined_yes_answers = [set(person).difference(set("\n")) for person in input_data]
 
-sum_of_yes = sum(combined_yes_answers)
+sum_of_yes = sum([len(group) for group in combined_yes_answers])
 
 print("\nThe sum of the combined 'Yes' answers is:", sum_of_yes)
+
+
+
+#****************** Part 2 *****
+
+
+# calculate number of questions everyone in a group answered with 'yes'
+
+all_yes = [person.split("\n") for person in input_data]
+
+all_yes_char_count = 0
+
+for group in all_yes:
+    joined = "".join(group)
+    all_yes_chars = list()
+    for char in joined:
+        if joined.count(char) == len(group) and char not in all_yes_chars:
+            all_yes_chars.append(char)
+    all_yes_char_count += len(all_yes_chars)
+
+print("\nThe sum of the all yes answers is:", all_yes_char_count) 
+    
