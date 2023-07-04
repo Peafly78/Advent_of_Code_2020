672bfe0edc59ca5661a6f331a711fcc13b9d2ad6
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-07-04 16:12:58 +0200

    Part 2 -- calculate number of required bags -- in progress

--- 07_luggage.py
+++ 07_luggage.py
@@ -16,7 +16,6 @@ rules_dict = {" ".join(rule[0].strip().split()[:2]) : rule[1].strip() for rule i
 
 # find shiny gold bag carry options
 
-carry_option_count = 0
 already_checked = list()
 check_list = ["shiny gold"]
 
@@ -28,4 +27,42 @@ while check_list:
             check_list.append(key)
 
 print(f"\nThere are {len(set(already_checked))-1} options to carry a shiny gold bag.")
+
+
+#****************** Part 2 *****
+
+
+# find out number of bags to buy
+
+print()
+print(rules_dict)
+
+counting_dict = {key : value.split(", ") for key, value in rules_dict.items()}
+
+print()
+print(counting_dict)
+print()
+
+# stuck here, finish later
+
+total_bags_needed = 0
+check_list_2 = [(1,"shiny gold")]
+temp_dict = dict()
+
+while check_list_2:
+    key_to_be_checked = check_list_2.pop(0)
+    for entry in counting_dict[key_to_be_checked]:
+        temp_list = entry.split()
+        if temp_list[0] == "no":
+            continue
+        multiplier = int(temp_list[0])
+        # bags_needed = int(temp_list[0])
+        next_color = " ".join(temp_list[1:3])
+        # total_bags_needed += bags_needed
+        check_list_2.append(next_color)
+        print(check_list_2)
+        print(total_bags_needed)
+        
+        
+
 
