6855676245d4fbf3dcd05afb6e5029edc6a23ac5
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-07-07 12:17:24 +0200

    Day 8: import and format data -- calculate accumulator value

---
+++ 08_handheld.py
@@ -0,0 +1,47 @@
+#***** ADVENT OF CODE 2020 *****
+#************ DAY 8 ************
+#****************** Part 1 *****
+
+
+# get input data
+
+input_data = None
+
+with open("08_handheld_input.txt") as input:
+    input_data = [tuple(input.strip().split()) for input in input.readlines()]
+
+# process instructions
+
+accumulator = 0
+idx = 0
+indices_visited = []
+
+while idx < len(input_data):
+    if idx in indices_visited:
+        break
+    indices_visited.append(idx)
+
+    command = input_data[idx][0]
+    move = input_data[idx][1]
+
+    if command == "nop":
+        idx += 1
+        continue
+
+    elif command == "acc":
+        if move[0] == "+":
+            accumulator += int(move[1:])
+        elif move[0] == "-":
+            accumulator -= int(move[1:])
+
+    elif command == "jmp":
+        if move[0] == "+":
+            idx += int(move[1:])
+            continue
+        elif move[0] == "-":
+            idx -= int(move[1:])
+            continue
+    
+    idx += 1
+
+print("\nThe value of the accumulator before entering an infinite loop is:", accumulator)
