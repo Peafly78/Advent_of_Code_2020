cb059a01f253f2d9750536651d9caa9a7b417f46
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-07-11 16:48:48 +0200

    fix remaining bugs

--- 11_seating.py
+++ 11_seating.py
@@ -11,17 +11,12 @@ input_data = None
 with open("11_seating_input.txt") as input:
     input_data = [input.strip("\n") for input in input.readlines()]
 
-print(input_data)
-
 # create seat class
 
 class Seat: 
     def __init__(self, occupied=False):
         self.occupied = occupied
         self.neighbors = list()
-    
-    def __repr__(self):
-        return "S"
     
     def add_neighbor(self, neighbor):
         self.neighbors.append(neighbor)
@@ -31,34 +26,32 @@ class Seat:
     
     def has_neighbors(self):
         for neighbor in self.neighbors:
-            if neighbor.is_occupied:
+            if neighbor.is_occupied():
                 return True
         return False
                 
     def has_four_neighbors(self):
         count_occupied = 0
         for neighbor in self.neighbors:
-            if neighbor.is_occupied:
+            if neighbor.is_occupied():
                 count_occupied += 1
         return count_occupied >= 4
     
     def change_occupation(self):
-        if self.is_occupied:
+        if self.is_occupied():
             self.occupied = False
         else:
             self.occupied = True
     
     def display(self):
-        if self.is_occupied:
-            return "#"
-        return "L"
+        if self.is_occupied():
+            print("#", end=" ")
+        else:
+            print("L", end=" ")
 
 # create seats according to grid
 
 seating_matrix = [[Seat() if spot == "L" else 0 for spot in row] for row in input_data]
-
-for row in seating_matrix:
-    print(row)
 
 # add neighbors where appropriate
 
@@ -89,55 +82,63 @@ for i in range(len(seating_matrix)):
                 continue
             seating_matrix[i][j].add_neighbor(spot)
 
-print()
-for row in seating_matrix:
-    for seat in row:
-        if seat == 0:
-            print(".", end=" ")
-        else:
-            print(len(seat.neighbors), end=" ")
-    print()
-print()
+# print()
+# for row in seating_matrix:
+#     for seat in row:
+#         if seat == 0:
+#             print(".", end=" ")
+#         else:
+#             print(len(seat.neighbors), end=" ")
+#     print()
+# print()
 
 # create function for changing seat status -- fix this
 
 def change_seats(matrix):
+    marked_for_change = list()
+
     for row in matrix:
         for seat in row:
             if seat == 0:
                 continue
             if seat.is_occupied() and seat.has_four_neighbors():
-                print("Changing to empty")
-                seat.change_occupation()
+                marked_for_change.append(seat)
             elif not seat.is_occupied() and not seat.has_neighbors():
-                print("Changing to occupied")
-                seat.change_occupation()
-    return matrix
+                marked_for_change.append(seat)
+    
+    for seat in marked_for_change:
+        seat.change_occupation()
+    
+    return len(marked_for_change) > 0
 
 # create function to print matrix
 
-
-
-# Testing
-
-print()
-for row in seating_matrix:
-    for seat in row:
-        if seat == 0:
-            print(".", end=" ")
-        else:
-            print(seat.display(), end=" ")
+def print_matrix(matrix):
     print()
-print()
-
-change_seats(seating_matrix)
-
-print()
-for row in seating_matrix:
-    for seat in row:
-        if seat == 0:
-            print(".", end=" ")
-        else:
-            print(seat.display(), end=" ")
+    for row in matrix:
+        for seat in row:
+            if seat == 0:
+                print(".", end=" ")
+            else:
+                seat.display()
+        print()
     print()
-print()
+
+# create function to count occupied seats
+
+def count_occupied_seats(matrix):
+    count = 0
+    for row in matrix:
+        for seat in row:
+            if seat == 0:
+                continue
+            if seat.is_occupied():
+                count += 1
+    return count
+
+# calculate occupied seats
+
+while change_seats(seating_matrix):
+    continue
+
+print("\nThe count of occupied seats is:", count_occupied_seats(seating_matrix))
