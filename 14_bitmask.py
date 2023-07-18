c5f454b1e56443942cd510de556bbf797360fedc
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-07-18 10:03:45 +0200

    fix: import data into different format for better processing

--- 14_bitmask.py
+++ 14_bitmask.py
@@ -11,27 +11,15 @@ input_data = None
 with open("14_bitmask_input.txt") as input:
     input_data = [input.strip("\n") for input in input.readlines()]
 
-print(input_data)
-
 mask_list = list()
 subdict = dict()
 
 for item in input_data: # fix this, data isn't input correctly
     if item.startswith("mask"):
-        mask_list.append(subdict)
-        subdict["mask"] = item[7:]
+        mask_list.append(item[7:])
     else:
         split = item.split(" = ")
-        subdict[split[0]] = int(split[1])
-
-mask_list.append(subdict)
-del subdict
-mask_list.pop(0)
-
-# for dic in mask_list:
-#     for key, value in dic.items():
-#         print(key, " > ", value)
-#     print()
+        mask_list.append({split[0] : int(split[1])})
 
 # calculate values and write them to memory
 
@@ -52,17 +40,14 @@ def convert_to_dec(bin_string):
     
 
 combined_memory = dict()
+bitmask = ""
 
 for item in mask_list:
-    bitmask = item["mask"]
-    print("Bitmask:", bitmask)
-    for key, value in item.items():
-        if key == "mask":
-            continue
-        combined_memory[key] = convert_to_dec(apply_mask(bitmask, str(convert_to_bin_36zeros(value))))
-
-for key, value in combined_memory.items():
-    print(key, " --> ", value)
+    if type(item) == dict:
+        for key, value in item.items():
+            combined_memory[key] = convert_to_dec(apply_mask(bitmask, str(convert_to_bin_36zeros(value))))
+    else:
+        bitmask = item
 
 # calculate sum of all values in combined memory
 
@@ -72,12 +57,3 @@ print("\nThe sum of all values in memory after completion of program initializat
 
 # Testing
 
-# dec_val = 11
-# bin_val = convert_to_bin_36zeros(dec_val)
-# print(bin_val)
-
-# changed_val = apply_mask(mask_list[0]["mask"], str(bin_val))
-# print(changed_val)
-
-# changed_dec_val = convert_to_dec(changed_val)
-# print(changed_dec_val)
