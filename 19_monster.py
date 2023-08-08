62988885de683edcf1e663ba4376762ef544916e
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2023-08-08 13:16:59 +0200

    feat: define helper functions to apply rules (in progress)

--- 19_monster.py
+++ 19_monster.py
@@ -27,4 +27,44 @@ for rule in formatting_rules:
         
 rules = {rule[0] : rule[1] for rule in formatting_rules}
 
-# continue here
+# apply rules to messages
+
+def rule_met(rule, letter):
+    if rule == letter:
+        return True
+    return False
+
+def is_reference(rule):
+    return isinstance(rule, int):
+
+def found_rule(rule):
+    return isinstance(rule, str)
+
+def message_compliant(message): # i guess it's too difficult for now
+    starting_rule_ref = 0
+    current_letter = message.pop(0)
+    current_rule = rules[starting_rule_ref]
+    sub_rules = None
+    while current_rule:
+        current_rule_part = current_rule.pop(0)
+        while not found_rule(current_rule_part):
+            while is_reference(current_rule_part):
+                current_rule_part = rules[current_rule_part]
+            else: 
+                sub_rules = current_rule_part
+                while sub_rules:
+                    sub_rule = sub_rules.pop(0)
+                    
+            
+        if rule_met(current_rule_part, current_letter):
+            current_letter = message.pop(0)
+            current_rule_part = current_rule.pop(0)
+        else:
+            return False
+    
+
+    return True
+
+    else:
+        current_rule = find_rule(current_rule)
+    return False
